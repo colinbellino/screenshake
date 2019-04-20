e650c311087bee469663ab020eb0aad6e9d9f170
Colin Bellino <bellino.colin@gmail.com>
2019-04-20 22:14:13 +0200

    ✨ Add player weapon

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -6,26 +6,15 @@ public class Move : MonoBehaviour
 
 	private IInput input;
 	private CharacterController2D controller;
-	private Animator animator;
 
 	private void Start()
 	{
 		input = GetComponent<IInput>();
 		controller = GetComponent<CharacterController2D>();
-		animator = GetComponent<Animator>();
 	}
 
 	private void FixedUpdate()
 	{
 		controller.Move(input.move * speed * Time.deltaTime, false, input.jump);
-
-		if (animator)
-		{
-			if (input.move != 0f)
-			{
-				animator.SetFloat("MoveX", input.move > 0f ? 1f : -1f);
-			}
-		}
-
 	}
 }
