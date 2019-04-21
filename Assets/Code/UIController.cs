c03b563d8f3bd696a59c74512dcba9541172f157
Colin Bellino <bellino.colin@gmail.com>
2019-04-21 23:08:37 +0200

    ✨ Add UI animation

---
+++ Assets/Code/UIController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+	[SerializeField] private TMPro.TextMeshProUGUI currentStepText;
+	[SerializeField] private SpriteRenderer backgroundRenderer;
+	[SerializeField] private Animator animator;
+
+	private DebugMenu debugMenu;
+
+	private void OnEnable()
+	{
+		debugMenu = GetComponent<DebugMenu>();
+
+		DebugMenu.OnStepChange += OnStepChange;
+	}
+
+	private void OnDisable()
+	{
+		DebugMenu.OnStepChange -= OnStepChange;
+	}
+
+	private void OnStepChange()
+	{
+		currentStepText.text = DebugMenu.steps[debugMenu.currentStepIndex].text;
+		backgroundRenderer.color = DebugMenu.steps[debugMenu.currentStepIndex].color;
+
+		Show();
+	}
+
+	private void Show()
+	{
+		animator.Play("Show");
+	}
+
+	private void Hide()
+	{
+		animator.Play("Hide");
+	}
+}
