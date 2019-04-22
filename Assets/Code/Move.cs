39bcb394e02a2b930305e72ff3f0994788e58b89
Colin Bellino <bellino.colin@gmail.com>
2019-04-22 21:02:47 +0200

    ✨ Add step11: enemy knockback

--- Assets/Code/Move.cs
+++ Assets/Code/Move.cs
@@ -3,26 +3,54 @@ using UnityEngine;
 public class Move : StepMonoBehaviour
 {
 	[SerializeField] private float speed = 1f;
+	[SerializeField] private bool knockback;
 
-	private float jumpTimestamp;
+	private Transform owner;
 	private const float jumpCooldown = 0.2f;
+	private const float hitStopDuration = 0.2f;
+	private float jumpTimestamp;
+	private float hitStopTimetamp;
+
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+
+		owner = transform.root;
+
+		Damage.OnDamage += OnDamage;
+	}
 
 	protected override void OnDisable()
 	{
 		base.OnDisable();
 
 		controller.Move(0f, false, false);
+
+		Damage.OnDamage -= OnDamage;
 	}
 
 	private void FixedUpdate()
 	{
 		var jumpCooldownElapsed = Time.time >= jumpTimestamp;
 
+		if (Time.time < hitStopTimetamp) { return; }
+
 		controller.Move(input.move * speed * Time.deltaTime, false, input.jump && jumpCooldownElapsed);
 
 		if (input.jump && jumpCooldownElapsed)
 		{
 			jumpTimestamp = Time.time + jumpCooldown;
+		}
+	}
+
+	private void OnDamage(Transform target, int damage)
+	{
+		if (target != owner) { return; }
+
+		if (knockback)
+		{
+			rb.AddForce(new Vector3(owner.right.x * -2f, 0, 0f), ForceMode2D.Impulse);
+			hitStopTimetamp = Time.time + hitStopDuration;
 		}
 	}
 }
